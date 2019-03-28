e109495d473772e14655db4d080643e16ecc2677
Maksim <maks112v@gmail.com>
2019-03-28 19:04:50 -0400

    Update main.js

--- roles/main.js
+++ roles/main.js
@@ -1,16 +1,29 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 module.exports = {
   getDishes,
-  findById,
+  addDish,
+  getDish,
+  getRecipes,
+  addRecipe
 };
 
 function getDishes() {
-  return db('dishes');
+  return db("dishes");
 }
 
 function addDish(dish) {
-  return db('dishes')
-    .where({ id })
-    .first();
+  return db("dishes").insert(dish);
+}
+
+function getDish(id){
+  return db('dishes as d').join('recipe as r','d.id', '=', 'r.dish_id').where('d.id', id)
+}
+
+function getRecipes(){
+  return db('recipes as r').join('dish as d','d.id', '=', 'r.dish_id')
+}
+
+function addRecipe(recipe){
+  return db('recipes').insert(recipe)
 }
